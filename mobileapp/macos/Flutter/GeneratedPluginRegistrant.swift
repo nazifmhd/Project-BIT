68272edc68e13ad5387c0df6d2eee25b566b07df
Mohamed Nazif <mohamednazif2001@gmail.com>
2025-03-17 14:36:12 +0530

    dependencies

--- mobileapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobileapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,26 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import cloud_firestore
+import cloud_functions
+import firebase_auth
 import firebase_core
+import firebase_ml_model_downloader
+import geolocator_apple
+import path_provider_foundation
+import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FirebaseModelDownloaderPlugin.register(with: registry.registrar(forPlugin: "FirebaseModelDownloaderPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
